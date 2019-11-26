736cc5627357bcdf87d54645f9e46c7788e82c53
RiaColaco <riacolaco@yahoo.com>
2019-11-26 23:39:22 +0530

    environment variable set

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,39 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  public apiURL = environment.siteURL;
+
   constructor(
     private _http: HttpClient
   ) { }
 
-  register(body:any){return this._http.post('http://127.0.0.2:3000/users/register',body,{
+  register(body:any){return this._http.post( this.apiURL + 'users/register',body,{
     observe:'body',
     headers: new HttpHeaders().append('Content-type','application/json')
   })}
 
-  login(body:any){return this._http.post('http://127.0.0.2:3000/users/login',body,{
-    observe:'body',
-    withCredentials: true,
-    headers: new HttpHeaders().append('Content-type','application/json')
-  })}
-
-  user(){return this._http.get('http://127.0.0.2:3000/users/user',{
+  login(body:any){return this._http.post( this.apiURL +'users/login',body,{
     observe:'body',
     withCredentials: true,
     headers: new HttpHeaders().append('Content-type','application/json')
   })}
 
-  books(){return this._http.get('http://127.0.0.2:3000/users/get-data',{
+  user(){return this._http.get( this.apiURL + 'users/user',{
     observe:'body',
     withCredentials: true,
     headers: new HttpHeaders().append('Content-type','application/json')
   })}
 
-  logout(){return this._http.get('http://127.0.0.2:3000/users/logout',{
+  books(){return this._http.get(this.apiURL +'users/get-data',{
+    observe:'body',
+    withCredentials: true,
+    headers: new HttpHeaders().append('Content-type','application/json')
+  })}
+
+  logout(){return this._http.get(this.apiURL +'users/logout',{
     observe:'body',
     withCredentials: true,
     headers: new HttpHeaders().append('Content-type','application/json')
